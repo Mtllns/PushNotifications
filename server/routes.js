31fba56cf641ea2131126e9da9262c9c48f43882
Mtllns <miquelmtllns@pm.me>
2020-06-04 11:36:03 +0200

    Add routes para:
    +suscripción
    +get key público
    +post push

--- server/routes.js
+++ server/routes.js
@@ -41,5 +41,29 @@ router.post('/', function (req, res) {
 });
 
 
+// Almacenar la suscripción
+router.post('/subscribe', (req, res) => {
+
+  res.json('subscribe');
+
+});
+
+// Enviamos key público al cliente para que luego pueda procesar y enviarnos la suscripción
+router.get('/key', (req, res) => {
+
+  res.json('key público');
+
+});
+
+// Enviar una notificación PUSH a las personas que nosotros queramos
+// ES algo que se controla del lado del server
+router.post('/push', (req, res) => {
+
+  res.json('key público');
+
+});
+
+
+
 
 module.exports = router;
